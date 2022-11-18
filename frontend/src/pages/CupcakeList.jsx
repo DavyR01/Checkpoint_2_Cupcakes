06d01af16305a980ccf3c84e1b2dcdfe86a14ea9
Davy ROBERT <davyrobert90@gmail.com>
2022-11-18 13:39:17 +0100

    update filters not finished

--- frontend/src/pages/CupcakeList.jsx
+++ frontend/src/pages/CupcakeList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable import/no-duplicates */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable react/button-has-type */
@@ -9,14 +10,42 @@ export default function CupcakeList() {
   // Step 1: get all cupcakes
 
   const [allCupcakes, setAllCupcakes] = useState([]);
+  const [selectedCupcakes, setSelectedCupcakes] = useState([]);
+  // const [filterCupCakes, setFilterCupcakes] = useState("");
 
+  // I didn't use a useEffect but a button to display cupcakes
+
+  // useEffect(() => {
   const getAllCupcakes = () => {
     fetch("http://localhost:4000/cupcakes")
       .then((reponse) => reponse.json())
       .then((result) => setAllCupcakes(result))
       .catch((err) => console(err));
+    console.log("API de tous les cupCakes");
   };
+  // });
+
   // Step 3: get all accessories
+
+  const getFilterCupcakes = () => {
+    fetch("http://localhost:4000/accessories")
+      .then((response) => response.json())
+      .then((result) => setSelectedCupcakes(result))
+      .catch((err) => console(err));
+    console.log("API de tous les accessoires");
+  };
+
+  // useEffect(() => {
+  //   const getFilterCupcakes = () => {
+  //     fetch("http://localhost:4000/accessories")
+  //       .then((response) => response.json())
+  //       .then((result) => setSelectedCupcakes(result))
+  //       .catch((err) => console(err));
+  //     console.log("API de tous les accessoires");
+  //   };
+  // });
+
+  // const handleSelectedCupcakes = (cake) => setSelectedCupcakes(cake);
 
   return (
     <>
@@ -24,9 +53,27 @@ export default function CupcakeList() {
       <form className="center">
         <label htmlFor="cupcake-select">
           Filter by{" "}
-          <select id="cupcake-select">
-            <option value="">---</option>
+          <select onClick={() => getFilterCupcakes()} id="cupcake-select">
             {/* Step 4: add an option for each accessory */}
+
+            {selectedCupcakes
+              // .filter((item) => item.includes({item.url}))
+              .map((item, index) => (
+                <option
+                  // eslint-disable-next-line react/no-array-index-key
+                  key={index}
+                  value={`${item.name}`}
+                  // onChange={() => setFilterCupcakes(item.url)}
+                >
+                  {item.name}
+                </option>
+              ))}
+
+            {/* <option value="Cherry">Cherry</option>
+            <option value="Donut">Donut</option>
+            <option value="chocolate">Chocolate</option>
+            <option value="Wild">Wild</option>
+            <option value="Christmas-Candy">Christmas Candy</option> */}
           </select>
         </label>
       </form>
@@ -35,8 +82,13 @@ export default function CupcakeList() {
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
         <li className="cupcake-item">
-          {allCupcakes.map((elem) => (
-            <Cupcake cupcake={elem} />
+          {allCupcakes.map((elem, index) => (
+            <Cupcake
+              cupcake={elem}
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              // handleSelectedCupcakes={handleSelectedCupcakes}
+            />
           ))}
         </li>
         {/* end of block */}
